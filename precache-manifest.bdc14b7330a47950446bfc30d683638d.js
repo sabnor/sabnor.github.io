cfdf9ce14b855a5cab1af8f095d81e36a86c49d4
Sabina Nordell <sabinanordell@Sabinas-MBP.lan>
2019-02-21 22:56:57 +0100

    Updates

---
+++ precache-manifest.bdc14b7330a47950446bfc30d683638d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0bfde97b34d33ed98303e5f07b9da4d",
+    "url": "/static/media/osqledARen.c0bfde97.png"
+  },
+  {
+    "revision": "9441870382dde191e4897c46f399aa6e",
+    "url": "/static/media/desk.94418703.png"
+  },
+  {
+    "revision": "7f1b222266d4ee4aafe15e4c824848b7",
+    "url": "/static/media/OL.7f1b2222.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7bd37b773a06ddf3f3fb",
+    "url": "/static/js/main.7bd37b77.chunk.js"
+  },
+  {
+    "revision": "3197c6674a6dfb62e4da",
+    "url": "/static/js/2.3197c667.chunk.js"
+  },
+  {
+    "revision": "7bd37b773a06ddf3f3fb",
+    "url": "/static/css/main.9a31c295.chunk.css"
+  },
+  {
+    "revision": "863b4f64e12f7448f4a1faaf616fc939",
+    "url": "/index.html"
+  }
+];
